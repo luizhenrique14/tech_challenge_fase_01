040a79ec86ae4b3db2df9651a294a4e4bdd6bfc2
Luiz Henrique <luiz.henrique1494@gmail.com>
2025-05-25 14:07:20 -0300

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ print('importando o dataset...')
 
 print(df.head())
 
-
 from sklearn.preprocessing import LabelEncoder
 
 label_encoder = LabelEncoder()
@@ -26,6 +25,53 @@ df['regiao'] = label_encoder.fit_transform(df['regiao'])
 
 print(df.head())
 
+print('Verificando os tipos de dados...')
+print(df.dtypes)
+
+
+
+print('Analisando os Graficos...')
+
+# # Criar o gráfico de boxplot idades
+# plt.boxplot(df['idade'])
+# plt.title('idade')
+# plt.ylabel('valores')
+# plt.show()
+
+
+# # Criar o gráfico de boxplot sexo
+# plt.boxplot(df['sexo'])
+# plt.title('sexo')
+# plt.ylabel('valores')
+# plt.show()
+
+# # Criar o gráfico de boxplot IMC
+# plt.boxplot(df['IMC'])
+# plt.title('IMC')
+# plt.ylabel('valores')
+# plt.show()
+
+
+# # Criar o gráfico de boxplot IMC
+# plt.boxplot(df['filhos'])
+# plt.title('filhos')
+# plt.ylabel('valores')
+# plt.show()
+
+
+# # Criar o gráfico de boxplot IMC
+# plt.boxplot(df['fumante'])
+# plt.title('fumante')
+# plt.ylabel('valores')
+# plt.show()
+
+# # Criar o gráfico de boxplot IMC
+# plt.boxplot(df['regiao'])
+# plt.title('regiao')
+# plt.ylabel('valores')
+# plt.show()
+
+
 # Separando os dados em treinos e testes
 from sklearn.model_selection import train_test_split
 
@@ -33,10 +79,11 @@ from sklearn.model_selection import train_test_split
 X = df.drop(columns=['fumante']) # Variáveis características
 y = df['fumante'] # O que eu quero prever. (Target)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=34)
 
 
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
 print('Treinando.......')
 # Utilizando - MinMaxScaler
 print('Treinando o modelo... MinMaxScaler')
@@ -47,6 +94,7 @@ scaler.fit(X_train)
 x_train_min_max_scaled = scaler.transform(X_train)
 x_test_min_max_scaled= scaler.transform(X_test)
 
+print(x_train_min_max_scaled)
 
 # Utilizando - StandardScaler
 print('Treinando o modelo... StandardScaler')
@@ -88,7 +136,7 @@ model_min_max.fit(x_train_min_max_scaled, y_train)
 y_pred_min_max = model.predict(x_test_min_max_scaled)
 
 accuracy_min_max = accuracy_score(y_test, y_pred_min_max)
-print(f'Acurácia: {accuracy_min_max:.2f}')
+print(f'Acurácia Min Max Scaler: {accuracy_min_max:.2f}')
 
 
 print('Testando com a padronização:')
@@ -101,7 +149,7 @@ model_standard.fit(x_train_standard_scaled, y_train)
 y_pred_standard = model.predict(x_test_standard_scaled)
 
 accuracy_strandard = accuracy_score(y_test, y_pred_standard)
-print(f'Acurácia: {accuracy_strandard:.2f}')
+print(f'Acurácia Standart Scaler: {accuracy_strandard:.2f}')
 
 # Meu modelo testato tanto com a padronização quanto com a normalização, ambos os métodos de escalonamento de dados, e ambos os métodos apresentaram resultados semelhantes.
 
@@ -133,6 +181,7 @@ print('Normalizando os dados utilizando o standardScaler.....')
 X = StandardScaler().fit_transform(X)
 #Visualizando nossos dados padronizados
 df_padronizado = pd.DataFrame(data=X, columns=features)
+print('df_padronizado')
 print(df_padronizado.head())
 
 # importando PCA da biblioteca sklearn
@@ -148,3 +197,107 @@ df_pca = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2', 'PC3'
 target = pd.Series(y, name='fumante')
 result_df = pd.concat([df_pca, target], axis=1)
 print(result_df.head())
+
+
+
+# Arvore de Decisao
+print('***********************************************************************************************************************')
+print('****************************************** Arvore de Decisao - Random Forest ******************************************')
+print('***********************************************************************************************************************')
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+from sklearn import tree
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import export_graphviz
+
+# Ajustar e transformar os rótulos
+print('Random Forest- Transformando os dados categóricos em numéricos...')
+df['sexo'] = label_encoder.fit_transform(df['sexo'])
+df['fumante'] = label_encoder.fit_transform(df['fumante'])
+df['regiao'] = label_encoder.fit_transform(df['regiao'])
+
+print(df.head())
+headers = ['idade','sexo','IMC','filhos','regiao','taxas', 'fumante']
+X = df[headers].values
+
+from sklearn.preprocessing import StandardScaler
+print('Normalizando os dados utilizando o standardScaler.....')
+# Normalizando os dados utilizando o standardScaler
+# (Padroniza as headers removendo a média e escala a variância a uma unidade.
+# Isso significa que para cada feature, a média seria 0, e o Desvio Padrão seria 1)
+X = StandardScaler().fit_transform(X)
+#Visualizando nossos dados padronizados
+df_padronizado = pd.DataFrame(data=X, columns=headers)
+print('df_padronizado')
+print(df_padronizado.head())
+
+print('Verificando os dados null...')
+print(df.isnull().sum()) # Verificando se existem dados nulos no dataset
+print('Ok sem dados null...')
+
+#analisando correlações
+print('Analisando as correlações entre as variáveis...')
+dados_coorelacionados = df.dropna()
+correlation_matrix = dados_coorelacionados.corr().round(2)
+fig, ax = plt.subplots(figsize=(7,7))
+sns.heatmap(data=correlation_matrix, annot=True, linewidths=.5, ax=ax)
+plt.show()
+
+#Separando os dados
+
+print('Removendo a colunar Target (fumante)...')
+x = df.drop(columns=['fumante'])
+y = df['fumante'] #O que eu quero prever. (Target)
+
+print('Separando os dados em treino e teste...')
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=48)
+
+print('Aplicando o algoritmo de arvore de decisão - RandomForestClassifier...')
+rf = RandomForestClassifier(n_estimators=5, max_depth = 2,  random_state=7)
+rf.fit(x_train, y_train)
+
+estimator = rf.estimators_
+
+
+y_predito_random_forest = rf.predict(x_test)
+
+# Metricas de precisão, revocação, f1-score e acurácia.
+print('RandomForestClassifier accuracy_score:', accuracy_score(y_test, y_predito_random_forest)) #relatório de validação das métrica de desempenho.
+
+
+class_names = ['Não Fumante', 'Fumante']
+label_names = ['idade', 'sexo',	'IMC',	'filhos',	'regiao',	'taxas']
+
+fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=800)
+tree.plot_tree(rf.estimators_[0],
+               feature_names = label_names,
+               class_names=class_names,
+               filled = True)
+fig.savefig('rf_individualtree.png')
+
+
+
+fig, axes = plt.subplots(nrows = 1,ncols = 5,figsize = (10,2), dpi=900)
+for index in range(0, 5):
+    tree.plot_tree(rf.estimators_[index],
+                   feature_names = label_names,
+                   class_names=class_names,
+                   filled = True,
+                   ax = axes[index])
+
+    axes[index].set_title('Estimator: ' + str(index), fontsize = 11)
+fig.savefig('rf_5trees.png')
+
+
+print('Base de Treino', rf.score(x_train, y_train))
+print('Base de Teste',rf.score(x_test, y_test))
+
